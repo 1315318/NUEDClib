27b1c5607e15c307cb2351de537494125d7f626f
hsy <1772034963@qq.com>
2026-07-13 22:56:49 +0800

    新增：MPU6050模块

--- MSPM0G3507/main.c
+++ MSPM0G3507/main.c
@@ -6,11 +6,13 @@
 #include "oled.h"
 #include "motor.h"
 #include "trace.h"
+#include "mpu_port.h"
 
 int status = 0;
 extern float target_speed_A;
 extern float target_speed_B;
 extern uint8_t trace_data[4];
+extern volatile uint32_t sys_tick_ms;
 
 static const char *reset_name(uint32_t cause) 
 {
@@ -24,6 +26,14 @@ static const char *reset_name(uint32_t cause)
     }
 }
 
+void _system_post_cinit(void) { }
+void __mpu_init(void) { }
+
+
+void SysTick_Handler(void) {
+    sys_tick_ms++;
+}
+
 int main(void)
 {
     SYSCFG_DL_init();
@@ -32,11 +42,22 @@ int main(void)
     OLED_Clear();
 
     uint32_t cause = (uint32_t)DL_SYSCTL_getResetCause();
-
     char buf[20];
     sprintf(buf, "RST:%s", reset_name(cause));
     OLED_ShowString(0, 0, (u8 *)buf, 16);
     OLED_Refresh();
+    delay_ms(500);
+
+    int dmp_ret = DMP_Init();
+    sprintf(buf, "DMP:%d", dmp_ret);
+    OLED_ShowString(0, 16, (u8 *)buf, 16);
+    OLED_Refresh();
+    if (dmp_ret != 0) {
+        while (1);
+    }
+
+    float pitch, roll, yaw;
+    char oled_str[20];
 
     motor_init(1);
     motor_init(2);
@@ -44,8 +65,17 @@ int main(void)
     target_speed_A = 0;
     target_speed_B = 0;
 
-    while (1) 
+    while (1)
     {
         trace_motor();
+        if (DMP_Read_Data(&pitch, &roll, &yaw) == 0) {
+            sprintf(oled_str,"p:%.2f",pitch);
+            OLED_ShowString(0, 16, (u8 *)oled_str, 16);
+            sprintf(oled_str,"r:%.2f",roll);
+            OLED_ShowString(0, 32, (u8 *)oled_str, 16);
+            sprintf(oled_str,"y:%.2f",yaw);
+            OLED_ShowString(0, 48, (u8 *)oled_str, 16);
+            OLED_Refresh();
+        }
     }
 }
